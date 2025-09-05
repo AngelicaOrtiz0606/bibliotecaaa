1e5f97fc55808e3a86af13e7f724213d9f801240
Angie <angelica.ortiz0606@gmail.com>
2025-09-05 10:44:53 -0600

    Modificamos los métodos de libroDomain y los de LibroController para interactuar entre la capa de dominio y presentación

--- biblioteca/src/main/java/com/fca/biblioteca/domain/LibroDomain.java
+++ biblioteca/src/main/java/com/fca/biblioteca/domain/LibroDomain.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 
@@ -14,7 +15,10 @@ public class LibroDomain {
     @Autowired
     private LibroRepository libroRepository;
 
-    private List<Libro> buscarLibroPorTitulo(String titulo){
-        return libroRepository.findAll();
+    public List<Libro> buscarLibroPorTitulo(String titulo, String edicion){
+        return libroRepository.findAll().stream()
+                .filter(libro -> libro.getTitulo().equals(titulo))
+                .filter(libro -> libro.getEdicion().equals(edicion))
+                .collect(Collectors.toList()); /*TAREA*/
     }
 }
--- biblioteca/src/main/java/com/fca/biblioteca/presentation/LibroController.java
+++ biblioteca/src/main/java/com/fca/biblioteca/presentation/LibroController.java
@@ -1,14 +1,23 @@
 package com.fca.biblioteca.presentation;
 
+import com.fca.biblioteca.data.Libro;
+import com.fca.biblioteca.domain.LibroDomain;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 
 @Controller
 public class LibroController {
+    @Autowired
+    private LibroDomain libroDomain;
+
     @RequestMapping(value = "/libros", method = RequestMethod.GET)
-    public String getLibros (){
-        return "Get some Libros";
+    public List<Libro> buscarLibrosPorTitulo (@RequestParam String titulo, @RequestParam String edicion){     /*Capa de presentación, que lo pasa a logica o usuario*/
+        return libroDomain.buscarLibroPorTitulo(titulo, edicion);   /*Aca ya traemos la lista*/
     }
 }
