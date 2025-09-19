b55cb59118a85dad1e8e7b11df3c7cd601563fbb
Angie <angelica.ortiz0606@gmail.com>
2025-09-19 10:26:14 -0600

    Cambiamos LibroController, con inyeccipon de dependencias y ResponseBody

--- biblioteca/src/main/java/com/fca/biblioteca/presentation/LibroController.java
+++ biblioteca/src/main/java/com/fca/biblioteca/presentation/LibroController.java
@@ -7,17 +7,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 
 @Controller
 public class LibroController {
-    @Autowired
+
+    /*Inyección de dependencias*/
     private LibroDomain libroDomain;
+    LibroController(LibroDomain libroDomain) {
+        this.libroDomain = libroDomain;
+    }
 
     @RequestMapping(value = "/libros", method = RequestMethod.GET)
-    public List<Libro> buscarLibrosPorTitulo (@RequestParam String titulo, @RequestParam String edicion){     /*Capa de presentación, que lo pasa a logica o usuario*/
+    @ResponseBody /*Lo convierte en un json*/
+    public List<Libro> buscarLibrosPorTitulo
+            (@RequestParam String titulo, @RequestParam String edicion){     /*Capa de presentación, que lo pasa a logica o usuario*/
         return libroDomain.buscarLibrosDisponibles(titulo, edicion);   /*Aca ya traemos la lista*/
     }
 }
